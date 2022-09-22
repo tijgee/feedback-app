29c90d02e0a2e8fb77d2ead31547ac2ec5603d15
Timothy J. Gee <1614043314117002@mil>
2022-09-21 17:01:03 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const title = "Blog Post";
+  const body = "This is my blog post";
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "Comment two" },
+    { id: 3, text: "Comment three" },
+  ];
+
+  const loading = false;
+  const showComments = true;
+  if (loading) return true;
+
+  const commentblock =  <div className="comments">
+  <h3>Comments ({comments.length})</h3>
+  <ul>
+    {comments.map((comment, index) => (
+      <li key={index}>{comment.text}</li>
+    ))}
+  </ul>
+</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{title}</h1>
+      <p>{body}</p>
+      {showComments && commentblock}
     </div>
   );
 }
